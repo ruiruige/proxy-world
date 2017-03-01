f553beb9712ca94f991a2e3e025b52cb1e8ec151
ruiruige <307443886@qq.com>
2017-03-01 11:34:46 +0800

    增加新文件

---
+++ vpn/ocserv/install_ocserv.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+sudo apt-get install libgnutls28-dev libwrap0-dev libpam0g-dev liblz4-dev libseccomp-dev libreadline-dev libnl-route-3-dev libkrb5-dev build-essential pkg-config gnutls-bin -y
+
+git clone https://gitlab.com/ocserv/ocserv.git
+
