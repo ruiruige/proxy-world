f973d0f54334656ffc9c0147047178564a96eeac
ruiruige <307443886@qq.com>
2017-03-03 11:05:48 +0800

    修正路径问题

--- common/network_optimize.sh
+++ common/network_optimize.sh
@@ -1,10 +1,13 @@
+#!/usr/bin/env bash
 # * soft nofile 51200 
 # * hard nofile 51200
-python3 ../utils/config_handler.py --set-config --config '* soft nofile' -r '* soft nofile 51200' -f /etc/security/limits.conf
-python3 ../utils/config_handler.py --set-config --config '* hard nofile' -r '* hard nofile 51200' -f /etc/security/limits.conf
+basepath=$(cd `dirname $0`; pwd)
+
+sudo python3 $basepath/../utils/config_handler.py --set-config --config '* soft nofile' -r '* soft nofile 51200' -f /etc/security/limits.conf
+sudo python3 $basepath/../utils/config_handler.py --set-config --config '* hard nofile' -r '* hard nofile 51200' -f /etc/security/limits.conf
 
 # session required pam_limits.so
-python3 ../utils/config_handler.py --set-config --config 'session required ' -r 'session required pam_limits.so' -f /etc/pam.d/common-session
+sudo python3 $basepath/../utils/config_handler.py --set-config --config 'session required ' -r 'session required pam_limits.so' -f /etc/pam.d/common-session
 
 # ulimit -SHn 51200
-python3 ../utils/config_handler.py --set-config --config 'ulimit -SHn ' -r 'ulimit -SHn 51200' -f /etc/profile
+sudo python3 $basepath/../utils/config_handler.py --set-config --config 'ulimit -SHn ' -r 'ulimit -SHn 51200' -f /etc/profile
