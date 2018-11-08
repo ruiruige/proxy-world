e0794114d6c632ad74f9932513ca3cd43063d605
git <307443886@qq.com>
2018-11-09 00:40:44 +0800

    准备合并分支

--- default/vimrc.vimrc
+++
@@ -1,6 +0,0 @@
-set pastetoggle=<F2>
-set nu
-set ts=4
-set expandtab
-set smartindent
-set hls
--- serverspeed/install_serverspeed.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-basepath=$(cd `dirname $0`; pwd)
-
-mkdir ~/install/serverspeed
-
-cp $basepath/serverspeeder-all.sh ~/install/serverspeed/
-
-sudo bash ~/install/serverspeed/serverspeeder-all.sh
