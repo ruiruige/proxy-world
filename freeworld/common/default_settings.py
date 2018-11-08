ea3d5c02f2d2d0d160293f4fc3926296110d83aa
git <307443886@qq.com>
2018-11-09 00:43:03 +0800

    准备合并分支

---
+++ freeworld/common/default_settings.py
@@ -0,0 +1,6 @@
+# /usr/bin/env python3
+# coding=utf-8
+
+def disable_iptables():
+    pass
+
---
+++ freeworld/common/network_optimize.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# * soft nofile 51200 
+# * hard nofile 51200
+basepath=$(cd `dirname $0`; pwd)
+
+sudo python3 ${basepath}/../utils/config_handler.py --set-config --config '* soft nofile' -r '* soft nofile 51200' -f /etc/security/limits.conf
+sudo python3 ${basepath}/../utils/config_handler.py --set-config --config '* hard nofile' -r '* hard nofile 51200' -f /etc/security/limits.conf
+
+# session required pam_limits.so
+sudo python3 ${basepath}/../utils/config_handler.py --set-config --config 'session required ' -r 'session required pam_limits.so' -f /etc/pam.d/common-session
+
+# ulimit -SHn 51200
+sudo python3 ${basepath}/../utils/config_handler.py --set-config --config 'ulimit -SHn ' -r 'ulimit -SHn 51200' -f /etc/profile
